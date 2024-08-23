127e85e192474a875e821e942ae7b652a9745e37
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-08-23 06:53:41 +0530

    create signup, connection to db, checking api calls

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+import User from '../models/user.modle.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async(req,res)=>{
+    const{username,email,password}=req.body;
+    const hashedPassword = bcryptjs.hashSync(password,10);
+    const newUser = new User({username,email,password :hashedPassword});
+    try {
+        await newUser.save()
+    res.status(201).json('User created Succesfully!!');
+        
+    } catch (error) {
+        res.status(500).json('ohh my gotto, a user already exists with these credentials  '+error.message);
+        
+    }
+    
+
+};
--- api/index.js
+++ api/index.js
@@ -2,14 +2,17 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=> {console.log('Connected to Mongodb!');})
 .catch((err)=>{
     console.log(err);
 });
 const app = express();
+app.use(express.json());// this will allow json as an input for this server.
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');}
 );
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
