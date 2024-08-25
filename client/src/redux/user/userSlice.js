968c0867a74cf481e8d23a8786bc10f88f177517
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-08-25 16:28:49 +0530

    added redux toolkit and persists to save data user in the browser

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false,
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState, // Corrected spelling
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => { // Corrected spelling
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        signInFailure: (state, action) => { // Corrected spelling
+            state.error = action.payload;
+            state.loading = false;
+        },
+    },
+});
+
+export const { signInStart, signInFailure, signInSuccess } = userSlice.actions;
+export default userSlice.reducer;
