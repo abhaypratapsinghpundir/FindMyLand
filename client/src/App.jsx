e3cdb79893508046724e2ce1a2e3acb66bf1ec36
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-08-29 02:10:23 +0530

    header updation and private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 
 export default function App() {
@@ -16,7 +17,10 @@ export default function App() {
       <Route path ="/about" element={<About/>} />
       <Route path ="/sign-in" element={<SignIn/>} />
       <Route path ="/sign-up" element={<SignUp/>} />
-      <Route path ="/profile" element={<Profile/>} />
+      <Route  element={<PrivateRoute/>}>
+        <Route path ="/profile" element={<Profile/>} />
+      </Route>
+
 
     </Routes>
     
