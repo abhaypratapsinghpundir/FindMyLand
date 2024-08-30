8d97d515d79fd140c7ef50aa6730b396dadae9e2
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-08-30 22:17:09 +0530

    updating the user image pt1

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
 
 export default function Profile() {
-  const {currentUser} = useSelector((state)=> state.user)
+  const fileRef = useRef(null)
+  const { currentUser} = useSelector((state)=> state.user)
   return (
     <div className='pd-3 max-width-lg mx-auto'>
       <h1 className=' text-3xl font-semibold text-center my-7'> Profile</h1>
       <form className=' flex flex-col gap-4'>
-        <img src={currentUser.avatar}alt="Profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self self-center' mt-2/>
+        <input type="file" ref={fileRef} hidden accept='image/*'/>
+
+        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar}alt="Profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self self-center' mt-2/>
         <input type="text" placeholder='UserName' id='username' className='border p-3 rounded-lg' />
         <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' />
         <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg' />
