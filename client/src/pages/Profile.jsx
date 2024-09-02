7c896c3096de56cd56ddba10ae0e8933a238434a
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-09-02 09:05:36 +0530

    Feature to delete user listing from the client side

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -122,6 +122,24 @@ const handleShowListings = async () => {
     setShowListingsError(true);
   }
 };
+const handleListingDelete = async (listingId) => {
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      console.log(data.message);
+      return;
+    }
+
+    setUserListings((prev) =>
+      prev.filter((listing) => listing._id !== listingId)
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+};
   return (
     <div className='pd-3 max-w-lg mx-auto'>
       <h1 className=' text-3xl font-semibold text-center my-7'> Profile</h1>
@@ -199,7 +217,12 @@ const handleShowListings = async () => {
               </Link>
 
               <div className='flex flex-col item-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+              <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
