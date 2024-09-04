e578378a189d019dd39770493a56e165abfee45b
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-09-05 04:31:53 +0530

    add pre installations

--- api/index.js
+++ api/index.js
@@ -5,17 +5,25 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=> {console.log('Connected to Mongodb!');})
 .catch((err)=>{
     console.log(err);
 });
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());// this will allow json as an input for this server.
 app.use(cookieParser());
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');}
 );
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
