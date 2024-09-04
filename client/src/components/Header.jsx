4ed52d140264f1c49f7e544494c44573f1b191ba
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-09-04 11:02:51 +0530

    header search form fucntionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-  const {currentUser} = useSelector(state =>state.user)
+  const {currentUser} = useSelector(state =>state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className='bg-green-400 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,11 +33,19 @@ export default function Header() {
             <span className ='text-red-950'>Land</span>
         </h1>
         </Link>
-        <form className='bg-green-300 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder =" Search..."
-             className='bg-transparent focus:outline-none w-24 sm:w-64' 
+        <form
+          onSubmit={handleSubmit}
+          className='bg-green-300 p-3 rounded-lg flex items-center'
+        >
+            <input type="text" 
+            placeholder =" Search..."
+            className='bg-transparent focus:outline-none w-24 sm:w-64' 
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <FaSearch className='text-green-700'></FaSearch>
+            <button>
+            <FaSearch className='text-green-600' />
+          </button>
         </form>
         <ul className='flex gap-4'>
             <Link to='/'> 
@@ -32,18 +58,17 @@ export default function Header() {
             </Link>
             <Link to='/profile'>
             {currentUser?(
-              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile"/>
-            ):(<li className=' hover:underline'>Sign in</li>
+              <img 
+              className='rounded-full h-7 w-7 object-cover'
+              src={currentUser.avatar}
+               alt="profile"/>
+            ):(
+            <li className=' hover:underline'>Sign in</li>
 
           )}
-            </Link>
-            
-            
-            
+            </Link>          
         </ul>
-
-        </div>
-        
-    </header>
+      </div>        
+  </header>
   )
 }
